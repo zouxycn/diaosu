1ac22974b0e65dd5bffde8f007ddd5f8c87c084c
张琪锋 <sealzqf@gmail.com>
2014-12-03 19:48:56 +0800

    添加用户作品列表页图片预览功能

--- js/user.js
+++ js/user.js
@@ -2,12 +2,46 @@ var DS = DS || {};
 DS.user = {};
 DS.user.work = {
 	init:function(){
-		//作品详情图片切换
-		this.initSlider();
 		//作品介绍显示
 		this.showAllText();
 		//回复按钮
 		this.showReplyBtn();
+	},
+	showAllText:function(){
+		var _box = $("#introText"),
+			_txtBox = _box.find(".uintro-text"),
+			_txt = _box.find(".uintro-text-box"),
+			_btn = _box.find(".show-all");
+
+		if(_txt.height() > 134)
+		{
+			_btn.removeClass("hide");
+			_btn.toggle(function(){
+				_btn.find("i").addClass("ico-up");
+				_btn.find("span").text("收起");
+				_txtBox.css("height","auto");
+			},function(){
+				_btn.find("i").removeClass("ico-up");
+				_btn.find("span").text("展开");
+				_txtBox.css("height","134px");
+			});
+		}
+	},
+	showReplyBtn:function(){
+		var _box = $("#userComment");
+		_box.find("li").hover(function(){
+			$(this).addClass("cur");
+		},function(){
+			$(this).removeClass("cur");
+		});
+	}
+};
+
+
+DS.user.album = {
+	init:function(){
+		//详情图片切换
+		this.initSlider();
 	},
 	initSlider:function(){
 		$("#workSlider").owlCarousel({
@@ -59,34 +93,6 @@ DS.user.work = {
 					//alert(_prev.css("top"))
 				}
 			}
-		});
-	},
-	showAllText:function(){
-		var _box = $("#introText"),
-			_txtBox = _box.find(".uintro-text"),
-			_txt = _box.find(".uintro-text-box"),
-			_btn = _box.find(".show-all");
-
-		if(_txt.height() > 134)
-		{
-			_btn.removeClass("hide");
-			_btn.toggle(function(){
-				_btn.find("i").addClass("ico-up");
-				_btn.find("span").text("收起");
-				_txtBox.css("height","auto");
-			},function(){
-				_btn.find("i").removeClass("ico-up");
-				_btn.find("span").text("展开");
-				_txtBox.css("height","134px");
-			});
-		}
-	},
-	showReplyBtn:function(){
-		var _box = $("#userComment");
-		_box.find("li").hover(function(){
-			$(this).addClass("cur");
-		},function(){
-			$(this).removeClass("cur");
 		});
 	}
 };
